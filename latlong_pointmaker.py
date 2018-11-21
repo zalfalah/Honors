e30152dec2a45d56567c07c278d2403fa12d6ae5
zalfalah <43095189+zalfalah@users.noreply.github.com>
2018-11-21 11:32:58 -0700

    Update latlong_pointmaker.py

--- latlong_pointmaker.py
+++ latlong_pointmaker.py
@@ -11,6 +11,22 @@ get_ipython().magic(u'matplotlib inline')
 import shapely as sp
 from shapely.geometry import Point
 def ll_pointmaker(dset, color = 'ro'):
+    """
+    Takes a dataset, creates points for each object with (x,y) values being represented by (Planetographic Latitude, Longitude) 
+    then graphs these points, defaults each point to be a red dot
+    
+    Parameters
+    __________
+    dset : dataframe
+        The dataframe from which we take our Lat/Long Coordinates
+    color : str
+        A color selection option for our resulting graph, default red dots
+        
+    Returns
+    _______
+    plot
+        Planetographic Latitude vs. Longitude for each object in our dataset
+    """
     dpoints = []
     for n in range(len(dset)):
         point = Point((dset.PlanetographicLatitude[n]),(dset.Longitude[n]))

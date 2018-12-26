dd5142b924414bb8bea30b27dd0f60e501b9d328
K.-Michael Aye <kmichael.aye@gmail.com>
2018-12-26 14:27:07 -0700

    fixed missing import in my_io. added debugging steps in notebook

--- my_io.py
+++ my_io.py
@@ -1,10 +1,13 @@
 import sys
 from pathlib import Path
+import pandas as pd
 
 if sys.platform == "darwin":
     rootpath = Path("/Users/klay6683/Dropbox/data/planet4/p4_for_Zade/P4_catalog_v1.1/")
 else:
     rootpath = Path("C:/Users/Zade/Documents/python_from_others/planet4_results/")
 
-fans = pd.read_csv(rootpath / "P4_catalog_v1.1_L1C_cut_0.5_fan.csv")
-blotches = pd.read_csv(rootpath / "P4_catalog_v1.1_L1C_cut_0.5_blotch.csv")
+def get_fans_blotches():
+    fans = pd.read_csv(rootpath / "P4_catalog_v1.1_L1C_cut_0.5_fan.csv")
+    blotches = pd.read_csv(rootpath / "P4_catalog_v1.1_L1C_cut_0.5_blotch.csv")
+    return fans, blotches

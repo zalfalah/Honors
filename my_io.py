b66e5783427e6f54c174a246972dde9daee9e3ec
zalfalah <43095189+zalfalah@users.noreply.github.com>
2018-12-31 19:04:51 -0600

    Update my_io.py
    changed path for Zade to reflect new laptop path

--- my_io.py
+++ my_io.py
@@ -5,7 +5,7 @@ import pandas as pd
 if sys.platform == "darwin":
     rootpath = Path("/Users/klay6683/Dropbox/data/planet4/p4_for_Zade/P4_catalog_v1.1/")
 else:
-    rootpath = Path("C:/Users/Zade/Documents/python_from_others/planet4_results/")
+    rootpath = Path("C:/Users/Zade Alfalah/Desktop/Python_Stuff/P4_catalog_v1.1")
 
 def get_fans_blotches():
     fans = pd.read_csv(rootpath / "P4_catalog_v1.1_L1C_cut_0.5_fan.csv")
